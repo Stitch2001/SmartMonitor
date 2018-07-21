cff917747bd338fe101280b0b0afa6dcb2dc6eeb
Cloudy <mr.shaber@formail.com>
2018-07-21 22:03:12 +0800

    完成了登录界面的外形

--- app/src/main/java/com/gdbjzx/smartmonitor/MainActivity.java
+++ app/src/main/java/com/gdbjzx/smartmonitor/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.avos.avoscloud.AVException;
@@ -18,7 +19,11 @@ import com.avos.avoscloud.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int ISLOGINED = 1;
+
     private DrawerLayout mDrawerLayout;
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +32,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_sort_by_size);
-        }
-        navigationView.setCheckedItem(R.id.nav_set_regulation);
+        }//设置Toolbar为默认ActionBar，设置图标
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        /*设置导航栏点击事件*/
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.nav_login:
                         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                        startActivity(intent);
-                    case R.id.nav_set_regulation:
-
+                        startActivityForResult(intent,ISLOGINED);
                     default:
                 }
                 mDrawerLayout.closeDrawers();
@@ -62,6 +65,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        navigationView.setCheckedItem(R.id.nav_monitor);//设置“检查”高亮
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    /*左侧菜单出现事件*/
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
@@ -69,5 +85,14 @@ public class MainActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_CANCELED) finish();
+            default:
+        }
     }
 }
