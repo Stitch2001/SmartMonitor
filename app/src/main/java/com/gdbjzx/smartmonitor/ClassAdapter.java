32cc21427257185f44f1688cae21a0c89a147450
Cloudy <mr.shaber@formail.com>
2018-08-04 21:36:49 +0800

    将设置检查顺序的界面设计完毕

--- app/src/main/java/com/gdbjzx/smartmonitor/ClassAdapter.java
+++ app/src/main/java/com/gdbjzx/smartmonitor/ClassAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.avos.avoscloud.search.SearchActivity;
@@ -18,35 +19,71 @@ import java.util.List;
  * Created by Administrator on 2018/8/3.
  */
 
-public class ClassAdapter extends ArrayAdapter<mClass> {
+public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> {
 
-    private int resourceId;
+    private List<mClass> mClassList;
 
-    public ClassAdapter(Context context, int textViewResourceId, List<mClass> objects){
-        super(context,textViewResourceId,objects);
-        resourceId = textViewResourceId;
-    }
+    static class ViewHolder extends RecyclerView.ViewHolder{
 
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        mClass mClass = getItem(position);
-        View view;
-        ViewHolder viewHolder;
-        if (convertView == null){
-            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-            viewHolder = new ViewHolder();
-            viewHolder.gradeText = (TextView) view.findViewById(R.id.grade_text);
-            view.setTag(viewHolder);
-        } else {
-            view = convertView;
-            viewHolder = (ViewHolder) view.getTag();
-        }
-        viewHolder.gradeText.setText(mClass.getGrade());
-        return view;
-    }
-
-    class ViewHolder {
+        View classView;
+        ImageView classImage1,classImage2,classImage3,classImage4,classImage5,classImage6,
+                classImage7,classImage8,classImage9,classImage10,classImage11,classImage12,
+                classImage13,classImage14,classImage15,classImage16,classImage17,classImage18;
         TextView gradeText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            classView = itemView;
+            classImage1 = (ImageView) itemView.findViewById(R.id.class_1);
+            classImage2 = (ImageView) itemView.findViewById(R.id.class_2);
+            classImage3 = (ImageView) itemView.findViewById(R.id.class_3);
+            classImage4 = (ImageView) itemView.findViewById(R.id.class_4);
+            classImage5 = (ImageView) itemView.findViewById(R.id.class_5);
+            classImage6 = (ImageView) itemView.findViewById(R.id.class_6);
+            classImage7 = (ImageView) itemView.findViewById(R.id.class_7);
+            classImage8 = (ImageView) itemView.findViewById(R.id.class_8);
+            classImage9 = (ImageView) itemView.findViewById(R.id.class_9);
+            classImage10 = (ImageView) itemView.findViewById(R.id.class_10);
+            classImage11 = (ImageView) itemView.findViewById(R.id.class_11);
+            classImage12 = (ImageView) itemView.findViewById(R.id.class_12);
+            classImage13 = (ImageView) itemView.findViewById(R.id.class_13);
+            classImage14 = (ImageView) itemView.findViewById(R.id.class_14);
+            classImage15 = (ImageView) itemView.findViewById(R.id.class_15);
+            classImage16 = (ImageView) itemView.findViewById(R.id.class_16);
+            classImage17 = (ImageView) itemView.findViewById(R.id.class_17);
+            classImage18 = (ImageView) itemView.findViewById(R.id.class_18);
+            gradeText = (TextView) itemView.findViewById(R.id.grade_text);
+        }
     }
+
+    public ClassAdapter(List<mClass> classList){
+        mClassList = classList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.class_item,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.classImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                mClass mClass = mClassList.get(position);
+                holder.classImage1.setImageResource(R.drawable.class_light_1);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        mClass mClass = mClassList.get(position);
+        holder.gradeText.setText(mClass.getGrade());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mClassList.size();
+    }
+
 }
--- app/src/main/java/com/gdbjzx/smartmonitor/SetRegulationActivity.java
+++ app/src/main/java/com/gdbjzx/smartmonitor/SetRegulationActivity.java
@@ -9,8 +9,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -31,12 +35,7 @@ public class SetRegulationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_regulation);
 
-        /*导入班级布局*/
-        initClass();//初始化班级数据
-        ClassAdapter adapter = new ClassAdapter(SetRegulationActivity.this,R.layout.class_item,classList);
-        ListView listItem = (ListView) findViewById(R.id.list_item);
-        listItem.setAdapter(adapter);
-
+        /*设置顶部栏*/
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -73,6 +72,13 @@ public class SetRegulationActivity extends AppCompatActivity {
             }
         });
 
+        /*导入班级布局*/
+        initClass();//初始化班级数据
+        RecyclerView listItem = (RecyclerView) findViewById(R.id.list_item);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);//设置线性布局
+        listItem.setLayoutManager(layoutManager);//指定布局
+        ClassAdapter adapter = new ClassAdapter(classList);//设置适配器
+        listItem.setAdapter(adapter);//加载适配器
     }
 
     /*点击按钮出现左侧菜单*/
@@ -80,6 +86,7 @@ public class SetRegulationActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
+                mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
